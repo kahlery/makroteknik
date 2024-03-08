61f4e962f63ce0fe852fb7d67ce154bc253167fb
KahlerYasla <garpayyasla@gmail.com>
2024-03-08 19:33:32 +0300

    1. responsive font size

--- web-client/src/components/HomeHero.jsx
+++ web-client/src/components/HomeHero.jsx
@@ -5,7 +5,7 @@ const HomeHero = () => {
     const imageUrl = process.env.PUBLIC_URL + `/heros/${index}.png`;
 
     return (
-        <div className="text-white bg-fixed lg:px-44 h-[29rem]"
+        <div className="text-white bg-fixed lg:px-44 max-[432px]:h-[500px] h-[450px] lg:h-[500px]"
             style={{
                 backgroundImage: `url(${imageUrl})`,
                 backgroundSize: 'cover',
@@ -13,19 +13,19 @@ const HomeHero = () => {
                 backgroundRepeat: 'no-repeat',
             }}
         >
-            <div className="absolute mx-4 sm:mx-24 lg:mx-0 top-[9.5rem] sm:top-[11rem] py-6 sm:py-8 lg:h-[14rem] lg:w-[30rem] overflow-hidden bg-black shadow-2xl shadow-black bg-opacity-[0.85]">
+            <div className="absolute mx-6 sm:mx-24 lg:mx-0 top-[12rem] py-6 sm:py-8 lg:h-[14rem] lg:w-[30rem] overflow-hidden bg-black shadow-2xl shadow-black bg-opacity-[0.85]">
                 <div className="flex flex-col justify-center">
                     <h2 className="text-base font-bold mb-2">
                         It's All About Trust...
                     </h2>
-                    <p className="text-sm mb-4 px-10">
+                    <p className="text-sm mb-4 px-4 sm:px-10 text-[0.7rem] sm:text-sm" style={{ color: '#f2f2f2' }} >
                         We are more than just a supplier of HVAC products. We are your partner in creating comfortable, efficient, and sustainable indoor environments.
                     </p>
-                    <div className="flex flex-row gap-4 justify-center px-10 text-sm">
+                    <div className="flex flex-row gap-4 justify-center px-4 sm:px-6 text-[0.7rem] sm:text-sm" style={{ color: '#f2f2f2' }} >
                         <button className="bg-primary text-white py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
                             Get An Offer
                         </button>
-                        <button className="border-2 text-white border-primary py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
+                        <button className="border-2 text-white border-secondary py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
                             Inspectate Our Products
                         </button>
                     </div>
--- web-client/src/components/LatestNewsGrid.jsx
+++ web-client/src/components/LatestNewsGrid.jsx
@@ -5,7 +5,7 @@ const LatestNewsGrid = () => {
     const imageUrl = process.env.PUBLIC_URL + `/patterns/${index}.png`;
 
     return (
-        <div className="text-white pb-14 lg:px-44"
+        <div className="text-white pb-14 px-6 lg:px-44"
             style={{
                 backgroundImage: `url(${imageUrl})`,
                 backgroundSize: '5%',
@@ -14,30 +14,30 @@ const LatestNewsGrid = () => {
             }}
         >
             <div className='flex flex-row justify-between items-center py-8' >
-                <p className="text-lg font-bold text-primary text-center shadow-lg px-4 py-1 backdrop-blur-sm rounded-lg">Latest Feeds:</p>
-                <button className="bg-primary text-white py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
+                <p className="text-sm lg:text-lg font-bold text-primary text-center shadow-lg px-4 py-1 backdrop-blur-sm rounded-lg">Latest Feeds:</p>
+                <button className="text-sm bg-primary text-white py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
                     List All Feeds
                 </button>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 sm:px-6 text-[0.7rem] sm:text-sm" style={{ color: '#f2f2f2' }} >
                 <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
                     <h3 className="text-sm font-bold mb-2">Headline lorem ipsum dolor sit amet...</h3>
                     <hr className="border-[0.1px] border-white border-opacity-30 mb-2" />
-                    <p className="text-sm">
+                    <p className="">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
                     </p>
                 </div>
                 <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
                     <h3 className="text-sm font-bold mb-2">Headline lorem ipsum dolor sit amet...</h3>
                     <hr className="border-[0.1px] border-white border-opacity-30 mb-2" />
-                    <p className="text-sm">
+                    <p className="">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
                     </p>
                 </div>
                 <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
                     <h3 className="text-sm font-bold mb-2">Headline lorem ipsum dolor sit amet...</h3>
                     <hr className="border-[0.1px] border-white border-opacity-30 mb-2" />
-                    <p className="text-sm">
+                    <p className="">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
                     </p>
                 </div>
