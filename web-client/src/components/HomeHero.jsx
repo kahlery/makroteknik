079ffee4015d8dae480aec72ba94f476871ee8fb
KahlerYasla <garpayyasla@gmail.com>
2024-03-08 16:20:58 +0300

    some responsive changes

--- web-client/src/components/HomeHero.jsx
+++ web-client/src/components/HomeHero.jsx
@@ -5,7 +5,7 @@ const HomeHero = () => {
     const imageUrl = process.env.PUBLIC_URL + `/heros/${index}.png`;
 
     return (
-        <div className="text-white bg-fixed lg:px-44 h-[30rem] pt-[102px] lg:pt-[122px]"
+        <div className="text-white bg-fixed lg:px-44 h-[29rem]"
             style={{
                 backgroundImage: `url(${imageUrl})`,
                 backgroundSize: 'cover',
@@ -13,15 +13,15 @@ const HomeHero = () => {
                 backgroundRepeat: 'no-repeat',
             }}
         >
-            <div className="absolute top-[11rem] py-8 h-[30%] w-[30%] overflow-hidden bg-black shadow-2xl shadow-black bg-opacity-80">
+            <div className="absolute mx-4 sm:mx-24 lg:mx-0 top-[9.5rem] sm:top-[11rem] py-6 sm:py-8 lg:h-[14rem] lg:w-[30rem] overflow-hidden bg-black shadow-2xl shadow-black bg-opacity-[0.85]">
                 <div className="flex flex-col justify-center">
                     <h2 className="text-base md:text-base font-bold mb-4">
                         It's All About Trust...
                     </h2>
-                    <p className="text-sm mb-8 px-10">
+                    <p className="text-sm mb-5 px-10">
                         We are more than just a supplier of HVAC products. We are your partner in creating comfortable, efficient, and sustainable indoor environments.
                     </p>
-                    <div className="flex flex-row gap-4 justify-center">
+                    <div className="flex flex-row gap-4 justify-center px-10">
                         <button className="bg-primary text-white py-2 px-4 rounded-lg shadow-lg hover:bg-secondary-dark">
                             Get An Offer
                         </button>
--- web-client/src/components/LatestNewsGrid.jsx
+++ web-client/src/components/LatestNewsGrid.jsx
@@ -1,24 +1,72 @@
 import React from 'react';
 
 const LatestNewsGrid = () => {
+    const index = 3; // or any other index value
+    const imageUrl = process.env.PUBLIC_URL + `/patterns/${index}.png`;
+
     return (
-        <div className='bg-blue-100'>
-            <p className="text-2xl font-bold text-primary mb-4">Latest News</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 px-4 lg:px-44 gap-12">
-                <div className="bg-white shadow-2xl p-2 h-64">
-                    <img src="https://via.placeholder.com/150" alt="placeholder" className="w-full h-40 object-cover mb-2" />
-                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for business</h3>
+        <div className="text-white pb-16 lg:px-44"
+            style={{
+                backgroundImage: `url(${imageUrl})`,
+                backgroundSize: '5%',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'repeat',
+            }}
+        >
+            <p className="text-lg font-bold text-primary text-start py-8">Latest Feeds:</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+                <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
+                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for...</h3>
+                    <hr className="border-[1px] border-secondary mb-2" />
+                    <p className="text-sm">
+                        We are now open for business. Our services include web development, mobile app development, and cloud computing.
+                        <span className="ml-2">
+                            <a href="#" className="text-secondary hover:underline">Read More</a>
+                        </span>
+                    </p>
                 </div>
-                <div className="bg-white shadow-2xl p-2 h-64">
-                    <img src="https://via.placeholder.com/150" alt="placeholder" className="w-full h-40 object-cover mb-2" />
-                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for business</h3>
+                <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
+                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for...</h3>
+                    <hr className="border-[1px] border-secondary mb-2" />
+                    <p className="text-sm">
+                        We are now open for business. Our services include web development, mobile app development, and cloud computing.
+                        <span className="ml-2">
+                            <a href="#" className="text-secondary hover:underline">Read More</a>
+                        </span>
+                    </p>
                 </div>
-                <div className="bg-white shadow-2xl p-2 h-64">
-                    <img src="https://via.placeholder.com/150" alt="placeholder" className="w-full h-40 object-cover mb-2" />
-                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for business</h3>
+                <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
+                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for...</h3>
+                    <hr className="border-[1px] border-secondary mb-2" />
+                    <p className="text-sm">
+                        We are now open for business. Our services include web development, mobile app development, and cloud computing.
+                        <span className="ml-2">
+                            <a href="#" className="text-secondary hover:underline">Read More</a>
+                        </span>
+                    </p>
+                </div>
+                <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
+                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for...</h3>
+                    <hr className="border-[1px] border-secondary mb-2" />
+                    <p className="text-sm">
+                        We are now open for business. Our services include web development, mobile app development, and cloud computing.
+                        <span className="ml-2">
+                            <a href="#" className="text-secondary hover:underline">Read More</a>
+                        </span>
+                    </p>
+                </div>
+                <div className="bg-black bg-opacity-80 p-4 shadow-2xl shadow-black">
+                    <h3 className="text-lg font-bold mb-2">Makro Tech LTD is now open for...</h3>
+                    <hr className="border-[1px] border-secondary mb-2" />
+                    <p className="text-sm">
+                        We are now open for business. Our services include web development, mobile app development, and cloud computing.
+                        <span className="ml-2">
+                            <a href="#" className="text-secondary hover:underline">Read More</a>
+                        </span>
+                    </p>
                 </div>
             </div >
-        </div >
+        </div>
     );
 };
 
