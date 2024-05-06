1a346777e8410920f754ff032db7d68725e91367
KahlerYasla <garpayyasla@gmail.com>
2024-05-06 14:41:54 +0300

    latest

---
+++ web-client/src/components/content/CategoriesQuickAccess.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Close } from '@mui/icons-material';
+
+const CategoriesQuickAccess = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('/data/categories.json')
+            .then((response) => response.json())
+            .then((data) => setCategories(data))
+    }, []);
+
+    return (
+        <div className="md:mx-64 mt-8 px-4 ">
+            <div className="flex flex-wrap justify-start gap-2 no-scrollbar overflow-x-scroll">
+                {categories.map((category) => (
+                    <button className="px-4 py-2 bg-gray-200 text-xs rounded-md hover:bg-gray-300">
+                        {category.categoryName}
+                        <Close sx={{ fontSize: '0.8rem' }} className="ml-1" />
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CategoriesQuickAccess;
--- web-client/src/pages/ProductsPage.jsx
+++ web-client/src/pages/ProductsPage.jsx
@@ -3,6 +3,7 @@ import ListingGrid from "../components/content/ListingGrid";
 import FilterSection from "../components/content/FilterSection";
 import { useCategoriesFilterStore } from '../stores/filter-stores/CategoriesFilterStore';
 import TopBanner from "../components/content/TopBanner";
+import CategoriesQuickAccess from "../components/content/CategoriesQuickAccess";
 
 const ProductsPage = () => {
     const [productsList, setProductsList] = useState([]);
@@ -22,6 +23,7 @@ const ProductsPage = () => {
         <div>
             <FilterSection />
             <TopBanner />
+            <CategoriesQuickAccess />
             <div className="w-screen mt-4 px-0 md:px-64 pt-4 pb-10 min-h-96">
                 <div className="mx-4">
                     <ListingGrid productsList={filteredProductsList} />
