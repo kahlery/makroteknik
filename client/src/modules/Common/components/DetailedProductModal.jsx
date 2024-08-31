07700a12da413c5d11312f5473986a917ff5f8fa
KahlerYasla <garpayyasla@gmail.com>
2024-08-31 16:29:20 +0300

    latest

--- client/src/modules/Common/components/DetailedProductModal.jsx
+++ client/src/modules/Common/components/DetailedProductModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 // icons
 import { ShoppingCart } from "@mui/icons-material"
-import { AiOutlineClose } from "react-icons/ai"
+import { IoIosArrowBack } from "react-icons/io"
 import { TbRulerMeasure } from "react-icons/tb"
 
 // stores
@@ -27,7 +27,7 @@ const DetailedProductModal = ({
 
     return (
         <div
-            className={`fixed left-0 top-0 w-screen h-[100vh] z-[10000] flex flex-col justify-center items-center transition-opacity duration-[.5s] gap-8 bg-black bg-opacity-75
+            className={`fixed left-0 top-0 w-screen h-[100svh] z-[10000] flex flex-col justify-center items-center transition-opacity duration-[.5s] bg-white bg-opacity-100 md:bg-black md:bg-opacity-75
                  ${
                      isModalOpen
                          ? "opacity-100"
@@ -40,7 +40,7 @@ const DetailedProductModal = ({
             {isModalOpen && selectedProduct && (
                 <>
                     <div
-                        className="relative shadow-2xl flex flex-col gap-2 mt-2 bg-white md:w-fit h-[80%] md:h-fit md:max-h-none p-3 mx-5 md:m-0 overflow-y-scroll"
+                        className="relative shadow-2xl flex flex-col gap-2 bg-white md:w-fit h-[100%] md:h-fit md:max-h-none p-4 pt-0 md:pt-4 md:m-0 overflow-y-scroll"
                         onClick={(e) => e.stopPropagation()}
                     >
                         <div className="flex flex-col md:flex-row h-fit">
@@ -52,6 +52,19 @@ const DetailedProductModal = ({
                                 alt={selectedProduct.title}
                                 className="hidden md:flex md:w-[40vw] max-h-[72svh] object-scale-down mr-0 mb-4 md:my-0 rounded-md border border-black border-opacity-20 shadow-md"
                             />
+                            <button
+                                className="sticky top-0 gap-1 z-50 md:hidden flex flex-row items-center bg-white w-full mb-2 py-4 border-b border-black border-opacity-20"
+                                onClick={() => {
+                                    setIsModalOpen(false)
+                                }}
+                            >
+                                <IoIosArrowBack
+                                    size="1.5rem"
+                                    fontWeight={"60rem"}
+                                    className="text-black"
+                                />
+                                <p className="text-black">Close</p>
+                            </button>
                             <div className="flex flex-col md:w-1/2 md:max-w-[35vw] md:max-h-[72svh] overflow-y-scroll md:justify-start gap-4 h-full md:ml-8 md:mr-4 md:py-3">
                                 <h2 className="text-xl text-black font-bold text-opacity-70">
                                     {selectedProduct.title}
@@ -73,7 +86,7 @@ const DetailedProductModal = ({
                                         }`}
                                     </p>
                                 </div>
-                                <div className="relative gap-4 md:gap-2 pb-2 flex overflow-x-scroll md:overflow-clip md:flex-wrap max-w-[80vw] md:max-w-[100%] justify-start mb-4">
+                                <div className="relative gap-4 md:gap-2 pb-2 flex overflow-x-scroll md:overflow-clip md:flex-wrap max-w-[85vw] md:max-w-[100%] justify-start mb-4">
                                     {selectedProduct.sizeToPrice.length > 1 ? (
                                         selectedProduct.sizeToPrice.map(
                                             (val, index) => {
@@ -144,13 +157,6 @@ const DetailedProductModal = ({
                                 className="flex md:hidden mt-4 object-cover mb-4 md:my-0 rounded-md border border-black border-opacity-20 shadow-md"
                             />
                         </div>
-                    </div>
-                    <div className="flex gap-3 md:hidden rounded-full py-[8px]">
-                        <AiOutlineClose
-                            size="2rem"
-                            fontWeight={"60rem"}
-                            className="text-white"
-                        />
                     </div>
                 </>
             )}
