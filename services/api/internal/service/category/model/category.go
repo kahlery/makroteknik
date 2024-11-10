37722c82d6abbc7ca9eca9094a7db84f9fef8eaf
KahlerYasla <garpayyasla@gmail.com>
2024-11-10 15:40:12 +0300

    update:service:*: loggers updated to get more detailed

--- services/api/internal/service/category/model/category.go
+++ services/api/internal/service/category/model/category.go
@@ -4,6 +4,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Category struct {
 	ID           primitive.ObjectID `bson:"_id" json:"id"`
-	CategoryID   string             `bson:"categoryID" json:"categoryID"`
 	CategoryName string             `bson:"categoryName" json:"categoryName"`
 }
--- services/api/internal/service/post/model/post.go
+++ services/api/internal/service/post/model/post.go
@@ -1,1 +1,9 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Category struct {
+	ID          primitive.ObjectID `bson:"_id" json:"id"`
+	PostContent string             `bson:"postContent" json:"postContent"`
+	PostTitle   string             `bson:"postTitle" json:"postTitle"`
+}
